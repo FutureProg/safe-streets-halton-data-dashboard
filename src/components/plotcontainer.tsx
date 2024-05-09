3eb56d1563377664fdd8442db03a0b7748338b1e
Nick Morrison <nickmorrison09@gmail.com>
2024-05-09 15:17:03 -0400

    Added Municipality dropdown to UI

--- src/components/plotcontainer.tsx
+++ src/components/plotcontainer.tsx
@@ -5,24 +5,23 @@ import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { selectFilters, setFilters } from "@/lib/features/filters/filtersSlice";
 import { useSelector } from "react-redux";
 import { clearLoadStates } from "@/lib/actions";
+import Multiselect from "multiselect-react-dropdown";
+import { MultiSelectStyles } from "@/types";
 
 export default ({ children,}: Readonly<{ children?: React.ReactNode; }>) => {
 
     let [isApplyButtonEnabled, setApplyButtonEnabled] = useState(false);
-    let [selectedYear, setSelectedYear] = useState(2023);
+    let [selectedYear, setSelectedYear] = useState(2023);    
     let filter = useAppSelector(selectFilters);
     let dispatch = useAppDispatch();
 
-    let yearDropdown = useRef<HTMLSelectElement>(null);
-
+    // When the filters are changed, re-enable the apply button
     useEffect(() => {
-      setApplyButtonEnabled(true);
+      setApplyButtonEnabled(selectedYear !== filter.year);
     }, [selectedYear])
-
-    let onChange = (sel: any) => {                                     
-        console.log(sel.target.value);
-        setSelectedYear(Number.parseInt(sel.target.value));
-    };
+    let onYearChange = (sel: any) => {                                           
+      setSelectedYear(Number.parseInt(sel.target.value));
+    };    
 
     let onApply = () => {  
       console.log(selectedYear);    
@@ -33,15 +32,50 @@ export default ({ children,}: Readonly<{ children?: React.ReactNode; }>) => {
       setApplyButtonEnabled(false);
     }
 
+    let cities = ['ACTON', 'BURLINGTON', 'GEORGETOWN', 'HALTON HILLS', 'MILTON', 'OAKVILLE'];        
+    let cityDropdownOptions = cities.map((cityName) => ({      
+      'value': cityName, 
+      'name': cityName.toLowerCase().split(" ").map((part) => part.charAt(0).toUpperCase() + part.substring(1)).join(" ")
+    }));
+    let [selectedCities, setSelectedCities] = useState(cityDropdownOptions);
+    let onCityChange = (selList: any, item: any) => {
+      console.log(selList);      
+    }
+    const multiSelectStyle : Partial<MultiSelectStyles> = {
+      multiselectContainer: {
+        display: 'inline-block'
+      }, 
+      chips: {
+        display: 'none'        
+      }                 
+    }    
+
     return (
       <div className={styles.plotContainer}>
         <div className={styles.filtersRow}>
           <div>
-            <label htmlFor="select-1">Year</label>
-            <select ref={yearDropdown} name="year" id="select-1" onChange={onChange} defaultValue={selectedYear}>
-              <option value={2023}>2023</option>
-              <option value={2024}>2024</option>            
-            </select>            
+            <span className={styles.filterItem}>
+              <label htmlFor="select-1">Year</label>
+              <select name="year" id="select-1" onChange={onYearChange} defaultValue={selectedYear}>
+                <option value={2023}>2023</option>
+                <option value={2024}>2024</option>            
+              </select>
+            </span>            
+            <span className={styles.filterItem}>
+              <label htmlFor="select-city">Municipality</label>
+              <Multiselect
+              style={multiSelectStyle}
+              showArrow={true}
+              className="inline-multi-select"
+              options={cityDropdownOptions}
+              selectedValues={selectedCities}
+              onRemove={onCityChange}
+              onSelect={onCityChange}            
+              displayValue="name"                        
+              showCheckbox={true}
+              />
+            </span>            
+            
             <button className="primary" disabled={!isApplyButtonEnabled} onClick={onApply}>Apply</button>
           </div>                    
         </div>      
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,17 @@
-import React from "react";
+import React, * as ReactLib from "react";
 
 export type DataPlot = React.ReactElement;
+
+
+/**
+ * Typedef for styles of the MultiSelectDropdown component, from https://www.npmjs.com/package/multiselect-react-dropdown
+ */
+export interface MultiSelectStyles {
+    multiselectContainer: ReactLib.CSSProperties
+    searchBox: ReactLib.CSSProperties
+    inputField: ReactLib.CSSProperties
+    chips: ReactLib.CSSProperties
+    optionContainer: ReactLib.CSSProperties
+    option: ReactLib.CSSProperties,
+    groupHeading: ReactLib.CSSProperties
+  }
