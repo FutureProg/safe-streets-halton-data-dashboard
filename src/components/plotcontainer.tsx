903734d13d8d39d3b97c60c260667ceeef566612
Nick Morrison <nickmorrison09@gmail.com>
2024-05-21 12:45:51 -0400

    Remove unused imports

--- src/components/plotcontainer.tsx
+++ src/components/plotcontainer.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import styles from './PlotContainer.module.css';
 import { LocalFilterContext, LocalFilterType, defaultFilters } from "@/contexts/LocalFilterContext";
 import { LocalFilters } from "./filters/LocalFilters";
