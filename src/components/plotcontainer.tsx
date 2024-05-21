f4c17237c41d85326f7eb4069ec2fff7ed7eedb4
Nick Morrison <nickmorrison09@gmail.com>
2024-05-21 12:45:39 -0400

    Remove unused imports

--- src/components/plotcontainer.tsx
+++ src/components/plotcontainer.tsx
@@ -1,13 +1,6 @@
 'use client';
 import React, { useContext, useEffect, useRef, useState } from "react";
 import styles from './PlotContainer.module.css';
-import { useAppDispatch, useAppSelector } from "@/lib/hooks";
-import { selectFilters, setFilters } from "@/lib/features/filters/filtersSlice";
-import { useSelector } from "react-redux";
-import { clearLoadStates } from "@/lib/actions";
-import Multiselect from "multiselect-react-dropdown";
-import { MultiSelectStyles } from "@/types";
-import { arrayDifference, arrayDisjoint } from "@/util";
 import { LocalFilterContext, LocalFilterType, defaultFilters } from "@/contexts/LocalFilterContext";
 import { LocalFilters } from "./filters/LocalFilters";
 import { ServerFilters } from "./filters/ServerFilters";
