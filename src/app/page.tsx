0fbf89eac5006209f4b6ddf8355c6464a7af8bd8
Nick Morrison <nickmorrison09@gmail.com>
2024-12-05 23:09:52 -0500

    Removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Image from "next/image";
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 // import styles from "./page.module.css";
 import './homepage.css';
 import dynamic from "next/dynamic";
 import ChartPlot from '../components/ChartPlot';
-import { PlotData, Data as PlotlyData } from 'plotly.js';
-import * as Api from '@/api';
-import { jsonArrayToPlotDataArr } from '@/util';
-import PlotContainer from "@/components/PlotContainer";
-import MapPlot from "@/components/MapPlot";
+import PlotContainer from "@/components/plotcontainer";
 import DataTable from "@/components/DataTable";
 
 export default function Home() { 
