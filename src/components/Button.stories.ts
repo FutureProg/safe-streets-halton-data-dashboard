c778cbe55055e8e590f2827014a07b2f9d5b68f0
Nick Morrison <nickmorrison09@gmail.com>
2024-12-15 21:58:10 -0500

    Added button component

--- src/components/Button.stories.ts
+++ src/components/Button.stories.ts
@@ -1,6 +1,6 @@
 import type {Meta, StoryObj} from '@storybook/react';
 
-import Button from './Button';
+import Button, { ButtonSize, ButtonVariant } from './Button';
 
 const meta: Meta<typeof Button> = {
     component: Button,
@@ -8,6 +8,23 @@ const meta: Meta<typeof Button> = {
         design: {
             type: 'figma',
             url: 'https://www.figma.com/design/LFm8nDtTmvSWZdJK3GtxRK/Safe-Streets-Dashboard?node-id=2011-552&t=6jObnkF3fr9QAHst-4'
+        },
+        layout: 'centered'
+    },
+    argTypes: {
+        size: {
+            options: ButtonSize,
+            control: {
+                type: 'select',
+                labels: Object.keys(ButtonVariant)
+            }
+        },
+        variant: {
+            options: ButtonVariant,
+            control: {
+                type: 'select',
+                labels: Object.keys(ButtonVariant)
+            }
         }
     }
 }
@@ -15,8 +32,42 @@ const meta: Meta<typeof Button> = {
 export default meta;
 
 type Story = StoryObj<typeof Button>;
-export const Default : Story = {
+export const Primary : Story = {
     parameters: {
-        
+    },
+    args: {
+        children: "Button Text",
+        size: "default",
+        variant: ButtonVariant.Primary
     }
 }
+
+export const Neutral: Story = {
+    args: {
+        children: "Button Text",
+        size: "default",
+        variant: "neutral"
+    },
+
+    parameters: {}
+};
+
+export const Secondary: Story = {
+    args: {
+        children: "Button Text",
+        size: "default",
+        variant: "secondary"
+    },
+
+    parameters: {}
+};
+
+export const Subtle: Story = {
+    args: {
+        children: "Button Text",
+        size: "default",
+        variant: "subtle"
+    },
+
+    parameters: {}
+};
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,59 @@
 import { PropsWithChildren } from "react"
+import classnames from 'classnames';
+import styles from './Button.module.scss';
 
-export default (props: PropsWithChildren) => {
+export enum ButtonVariant {
+    Primary = 'primary',
+    Secondary = 'secondary',
+    Neutral = 'neutral',
+    Subtle = 'subtle'
+}
+
+export enum ButtonSize {
+    Default = 'default',
+    Small = 'small'
+}
+
+type Props = {
+    variant: ButtonVariant;
+    size?: ButtonSize;
+}
+
+export type ButtonProps = PropsWithChildren<Props>;
+
+export default ({
+    variant = ButtonVariant.Subtle,
+    size = ButtonSize.Small,
+    children = ""
+}: ButtonProps) => {
+    const variantStyle = () => {
+        switch (variant) {
+            case ButtonVariant.Primary:
+                return styles.variantPrimary;
+            case ButtonVariant.Secondary:
+                return styles.variantSecondary;
+            case ButtonVariant.Neutral:
+                return styles.variantNeutral;
+            case ButtonVariant.Subtle:
+                return styles.variantSubtle;
+        }
+    };
+    const sizeStyle = () => {
+        switch(size) {
+            case ButtonSize.Default:
+                return "";
+            case ButtonSize.Small:
+                return styles.sizeSmall;
+        }
+    }
+
+    const classNames = classnames(styles.view, 
+        variantStyle(),
+        sizeStyle()
+    );
     return (
-        <button>{props.children}</button>
+        <button className={classNames}>
+            {children}
+        </button>
     )
 }
