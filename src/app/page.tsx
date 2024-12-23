43da14440ecd026437471b159eee8cf8c1a93676
Nick Morrison <nickmorrison09@gmail.com>
2024-12-23 18:23:04 -0500

    Added Menu panel to the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 'use client';
 import React from 'react';
 import BaseMap from '@/components/BaseMap';
+import styles from './page.module.scss';
+import Panel from '@/components/Panel';
+import ToggleButton from '@/components/ToggleButton';
 
-export default function Home() { 
+import MenuToggleIcon from '@/img/bars.svg';
+import Image from 'next/image';
+
+export default function Home() {
+
+  const menuIcon = <Image priority src={MenuToggleIcon} alt='Menu button' width='18' height='18' />;
+
   return (
     <main>
-      <BaseMap></BaseMap>
+      <div className={styles.mapContainer}>
+        <BaseMap></BaseMap>
+      </div>    
+      <div className={styles.contentView}>
+        <div className={styles.columnContainer}>
+          <Panel>
+            <div className={styles.menuPanel}>
+              <span className={styles.title}>Safe Streets Dashboard</span>
+              <ToggleButton icon={menuIcon} alt='Menu button' aria-haspopup='menu' aria-controls='menu-popup' aria-expanded='false'></ToggleButton>
+            </div>
+          </Panel>
+        </div>
+      </div>  
     </main>
   );
 }
