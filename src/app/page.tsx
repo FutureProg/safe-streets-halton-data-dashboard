1d7b3cbd6f53e3fea1f0ac0b501f3cb4d21d1293
Nick Morrison <nickmorrison09@gmail.com>
2024-12-23 14:05:29 -0500

    Moved map plot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
+'use client';
 import React from 'react';
 // import styles from "./page.module.css";
+import Map from '@/components/MapPlot'
 import './homepage.css';
-import dynamic from "next/dynamic";
 import ChartPlot from '../components/ChartPlot';
 import PlotContainer from "@/components/plotcontainer";
 import DataTable from "@/components/DataTable";
 
 export default function Home() { 
-
-  const Map = React.useMemo( () => dynamic(
-    () => import('@/components/MapPlot'),
-    { ssr: false }
-  ), []);
 
   return (
     <main>
