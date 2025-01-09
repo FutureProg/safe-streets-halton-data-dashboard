7462009283ea820e66ebbaa1b4dc0d7ddac6c040
Nick Morrison <nickmorrison09@gmail.com>
2025-01-09 15:30:41 -0500

    Removing unused variables

--- src/app/api/data/info/route.ts
+++ src/app/api/data/info/route.ts
@@ -1,10 +1,8 @@
 import { findInfo } from "@/db/db";
-import { NextRequest } from "next/server";
 
 export const dynamic = 'force-dynamic';
 
-export const GET = async (request: NextRequest) => {
-    const searchParams = request.nextUrl.searchParams;
+export const GET = async () => {
     const result = await findInfo();
     return Response.json(result);
 }
