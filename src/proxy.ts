9ddd58c2eae11a577b808595e75bab9baccf02f9
Nick Morrison <nickmorrison09@gmail.com>
2025-11-29 18:01:53 -0500

    Update nextjs version

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import {i18nRouter} from 'next-i18n-router';
 import i18nConfig from './i18nConfig';
 import { NextRequest } from 'next/server';
 
-export const middleware = (request: NextRequest) => {
+export const proxy = (request: NextRequest) => {
     return i18nRouter(request, i18nConfig);
 }
 
