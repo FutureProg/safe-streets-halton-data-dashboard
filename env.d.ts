506b0e7745c788180512b129f3fcd189ef4b4339
Nick Morrison <nickmorrison09@gmail.com>
2026-09-11 18:02:42 -0400

    Update to env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -63,13 +63,21 @@ export type CoercedEnvSchema = {
    */
   PORT?: number;
   
+  /**
+   * **dataAPI**  
+   * The endpoint running data from, set via nextconfig.mjs  
+   * DO NOT SET  
+   * ![icon](data:image/svg+xml;utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2220%22%20height%3D%2220%22%20viewBox%3D%220%200%2032%2032%22%3E%3Cpath%20fill%3D%22%23808080%22%20d%3D%22M29%2022h-5a2.003%202.003%200%200%201-2-2v-6a2%202%200%200%201%202-2h5v2h-5v6h5ZM18%2012h-4V8h-2v14h6a2.003%202.003%200%200%200%202-2v-6a2%202%200%200%200-2-2m-4%208v-6h4v6Zm-6-8H3v2h5v2H4a2%202%200%200%200-2%202v2a2%202%200%200%200%202%202h6v-8a2%202%200%200%200-2-2m0%208H4v-2h4Z%22%2F%3E%3C%2Fsvg%3E)   
+   */
+  dataAPI?: string;
+  
 };
 
-type _CoercedEnvSchema_c8f8b452 = CoercedEnvSchema;
+type _CoercedEnvSchema_2895587b = CoercedEnvSchema;
 
 declare module 'varlock/env' {
-  export interface TypedEnvSchema extends Readonly<_CoercedEnvSchema_c8f8b452> {}
-  export interface PublicTypedEnvSchema extends Readonly<Pick<_CoercedEnvSchema_c8f8b452, 'CI' | 'DB_HOST' | 'DB_NAME' | 'DB_PORT' | 'DB_USER' | 'PORT'>> {}
+  export interface TypedEnvSchema extends Readonly<_CoercedEnvSchema_2895587b> {}
+  export interface PublicTypedEnvSchema extends Readonly<Pick<_CoercedEnvSchema_2895587b, 'CI' | 'DB_HOST' | 'DB_NAME' | 'DB_PORT' | 'DB_USER' | 'PORT' | 'dataAPI'>> {}
 }
 
 
@@ -79,17 +87,17 @@ export type EnvSchemaAsStrings = {
       : (NonNullable<CoercedEnvSchema[Property]> extends boolean ? ('true' | 'false') : string)
 };
 
-type _EnvSchemaAsStrings_c8f8b452 = EnvSchemaAsStrings;
+type _EnvSchemaAsStrings_2895587b = EnvSchemaAsStrings;
 declare global {
 
   // add types for global import.meta.env
-  interface ImportMetaEnv extends _EnvSchemaAsStrings_c8f8b452 {}
+  interface ImportMetaEnv extends _EnvSchemaAsStrings_2895587b {}
   interface ImportMeta {
     readonly env: ImportMetaEnv;
   }
 
   // add types for global process.env
   namespace NodeJS {
-    interface ProcessEnv extends _EnvSchemaAsStrings_c8f8b452 {}
+    interface ProcessEnv extends _EnvSchemaAsStrings_2895587b {}
   }
 }
